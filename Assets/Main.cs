9306621eba42e9d81a65ee5733407699bd260671
Dondi <dondi@dondi.net>
2016-06-05 16:03:10 -0400

    Added more script

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -6,6 +6,7 @@ public class Main : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("Zombies are Chasing Me");
+		print ("...bit I've managed to escape");
 	}
 	
 	// Update is called once per frame
